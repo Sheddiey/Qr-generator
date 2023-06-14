9ec07a92d0ecfd5b9c1a1d4fcf1f6fc19856cb8d
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-06-14 08:24:52 +0300

    if else

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 
 function showIt () {
-    document.getElementById("alert").src = "https://pbs.twimg.com/profile_images/1668224238925258754/yJfuNK9q_400x400.jpg";
+    var text = document.getElementById("myOption").value;
+    var image = document.getElementById("myImage");
+    if(text == 'Sheddiey'){
+        image.src = "https://pbs.twimg.com/profile_images/1668224238925258754/yJfuNK9q_400x400.jpg"
+    }
+    else if(text =="Google"){
+        image.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/800px-Google_2015_logo.svg.png"
+    }
+    else if (text == "Facebook"){
+        image.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Facebook.svg/2560px-Facebook.svg.png"
+    }
+    else if (text == "Bing"){
+        image.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Bing_logo_%282016%29.svg/1200px-Bing_logo_%282016%29.svg.png"
+    }
+    else{
+        alert("No Picture Available");
+    };
 };
